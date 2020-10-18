74c76d563d50c58069a7f4c0cea0faca9b010b39
Yaxita Patel <yaxita.patel.8@gmail.com>
2020-10-17 21:28:37 -0700

    Change Route controller

--- Startup.cs
+++ Startup.cs
@@ -50,7 +50,7 @@ namespace LabYaxita
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=File}/{action=Index}/{id?}");
             });
         }
     }
